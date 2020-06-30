81b57a7b58981cadeb3e3dc6f1509d9b3c588474
Theodore Verge <theodore.verge@gmail.com>
2020-06-30 17:12:27 +0100

    ...

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -4,7 +4,15 @@ export default class extends Controller {
   static targets = [ "source" ]
 
   copy() {
+    event.preventDefault()
     this.sourceTarget.select()
     document.execCommand("copy")
+  }
+
+
+  connect() {
+    if (document.queryCommandSupported("copy")) {
+      this.element.classList.add("clipboard--supported")
+    }
   }
 }
